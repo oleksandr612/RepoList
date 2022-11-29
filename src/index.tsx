9115d360a3d4017bb6ae751fd92991c6095cf030
oleksandr612 <kurylovoleksandr@ukr.net>
2022-11-29 10:09:37 +0900

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Index = () => {
+  return <div>This is the Redux-TypeScript Example</div>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Index />, document.getElementById("root"));
